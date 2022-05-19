ea8dfed917877ee8d385e38528cca23910fcc0b8
bogach-ivan <blackview1617@gmail.com>
2022-05-19 20:03:47 +0300

    asfsadfsdf

--- services/auth/repo/auth_mysql.go
+++ services/auth/repo/auth_mysql.go
@@ -20,18 +20,31 @@ func NewAuthMySQL(db *sql.DB) *AuthMySQL {
 	}
 }
 
+// TODO perform setting of db connection timeout and so on
 func (r *AuthMySQL) CreateUser(ctx context.Context, user *pb.User) (*pb.CreateUserResponse, error) {
-	var id int
+	// var id int
 	query := fmt.Sprintf("INSERT INTO %s (username, email, password, expires, type) values (?, ?, ?, ?, ?)", usersTable)
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return &pb.CreateUserResponse{Id: 0}, err
+	}
+	defer stmt.Close()
 
 	var datetime = time.Now()
 	t2 := datetime.AddDate(0, 0, 7)
 	dt := t2.Format(time.RFC3339)
-	fmt.Println(query, user.Username, user.Email, user.Password, dt, "free")
-	row := r.db.QueryRow(query, user.Username, user.Email, user.Password, dt, "free")
-	err := row.Scan(&id)
+	res, err := stmt.ExecContext(ctx, user.Username, user.Email, user.Password, dt, "free")
 	if err != nil {
+		fmt.Println("Error: " + err.Error())
 		return &pb.CreateUserResponse{Id: 0}, err
 	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return &pb.CreateUserResponse{Id: 0}, err
+	}
+
+	fmt.Printf("ID: %d", id)
 	return &pb.CreateUserResponse{Id: int32(id)}, nil
 }
